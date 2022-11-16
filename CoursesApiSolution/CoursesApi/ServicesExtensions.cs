1237a1523e9003e0c14503abed2186d82022ef27
sherenegladyssamuel <116015523+sherenegladyssamuel@users.noreply.github.com>
2022-11-16 11:51:50 -0500

    Initial commit

---
+++ CoursesApiSolution/CoursesApi/ServicesExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace CoursesApi;
+
+public static class ServicesExtensions
+{
+    public static void AddCoursesServices(this IServiceCollection services)
+    {
+        services.AddTransient<IProvideOfferings, OfferingsCatalog>();
+        services.AddTransient<CourseCatalog>();
+        // etc. etc.
+    }
+}
